ca3635b943e344a02232441e28c2f76dd548bc8a
a.hackman@zoovu.com <a.hackman@zoovu.com>
2022-11-17 19:29:37 +0000

    Updated connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,9 @@
 <?php
+$dir = '/';
+$files1 = scandir($dir);
+
+
+/*
 //$mysqli = new mysqli("localhost","root","","lms");
 // Check connection
 //zoovu
@@ -6,4 +11,8 @@
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "/cert/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "zoovudemoshop.mysql.database.azure.com", "zoovu", "Z00vu1234", "demoshop", 3306, MYSQLI_CLIENT_SSL);
+*/
+
+
+
 ?>
