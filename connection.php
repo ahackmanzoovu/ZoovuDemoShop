fd215dc1b7bc7e53dc545a12d51641f3c0df631f
a.hackman@zoovu.com <a.hackman@zoovu.com>
2022-11-17 20:31:26 +0000

    Updated connection.php v13 142132

--- connection.php
+++ connection.php
@@ -9,9 +9,9 @@ error_reporting(E_ALL);
 //Z00vu1234
 
 $cert = $_SERVER['DOCUMENT_ROOT'] . "/cert/DigiCertGlobalRootCA.crt.pem";
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, $cert , NULL, NULL);
-mysqli_real_connect($con, "zoovudemoshop.mysql.database.azure.com", "zoovu", "Z00vu1234", "demoshop", 3306, MYSQLI_CLIENT_SSL);
+$mysqli = mysqli_init();
+mysqli_ssl_set($mysqli,NULL,NULL, $cert , NULL, NULL);
+mysqli_real_connect($mysqli, "zoovudemoshop.mysql.database.azure.com", "zoovu", "Z00vu1234", "demoshop", 3306, MYSQLI_CLIENT_SSL);
 
 
 
