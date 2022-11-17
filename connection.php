a21ca0272b2b94447f941c879033126049e8e9b0
a.hackman@zoovu.com <a.hackman@zoovu.com>
2022-11-17 20:22:36 +0000

    4342Updated connection.php

--- connection.php
+++ connection.php
@@ -1,11 +1,12 @@
 <?php
+/*
 // Check connection
 //zoovu
 //Z00vu1234
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "/cert/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "zoovudemoshop.mysql.database.azure.com", "zoovu", "Z00vu1234", "demoshop", 3306, MYSQLI_CLIENT_SSL);
-
+*/
 
 
 
