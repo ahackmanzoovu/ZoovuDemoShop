5ab3a8bbc01923a0d8a565eeb3f32b3bdf3845b4
a.hackman <a.hackman@zoovu.com>
2022-12-05 17:54:05 -0600

    new file:   returns.php

---
+++ returns.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="container text-center">
+        <div>
+            <h2 style="text-align: center;">HOW RETURNS WORK<br></h2>
+            <p style="text-align: center;">Our team is happy to help with returns for products you purchased through Whirlpool.com. Simply call <a href="tel:18666982538">1 (866) 698-2538</a> for major appliances and major appliance parts &amp; accessories, <a href="tel:18005416390">1 (800) 541-6390</a> for water filters, or <a href="tel:18006430961">1 (800) 643-0961</a> for appliance cleaners. Have your order number or product serial number handy and we can help you through the process.</p>
+            <p style="text-align: center;"><sub>NOTE: We only accept returns for products purchased directly from Whirlpool.com. If you purchased a product at another retailer, please contact that retailer regarding your return.</sub></p>
+        </div>
+    </div>
+</div>
